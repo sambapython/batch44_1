dd3e97774a1a68a0b24d32641b23f90c3aa433c2
sambapython@gmail.com <sambapython@gmail.com>
2018-03-30 08:36:21 +0530

    2018-03-30

--- modules/app.py
+++ modules/app.py
@@ -2,10 +2,57 @@
 print "a=%s, b=%s, c=%s"%(a,b,c)
 fun()
 '''
+'''
+import sys
+print sys.path
 
 import f1
 import f2
+
 print "in f1: a=%s, b=%s, c=%s"%(f1.a,f1.b,f1.c)
 f1.fun()
 print "in f2: a=%s, b=%s, c=%s,d=%s"%(f2.a,f2.b,f2.c,f2.d)
 f2.fun()
+'''
+'''
+import f3
+f3.fun()
+'''
+'''
+import sys
+sys.path.append("/home/khyaathi-python")
+print sys.path
+import f4
+import f3
+f3.fun()
+'''
+'''
+import sys
+#sys.path.append("/home/khyaathi-python")
+sys.path.insert(1,"/home/khyaathi-python")
+print sys.path
+import f4
+import f3
+f3.fun()
+'''
+'''
+import sys
+import f3
+sys.path.insert(1,"/home/khyaathi-python")
+import f3
+import f3
+'''
+'''
+import f1
+f1.fun()
+f1.fun1()
+'''
+'''
+from f1 import fun
+fun()
+'''
+
+import f1
+f1.fun()
+print "a=",f1.a
+f1.fun2()
--- modules/f1.py
+++ modules/f1.py
@@ -2,7 +2,14 @@ a=10
 b=20
 c=a+b
 def fun():
-	print "this is fun in f1"
+	print "this is fun in f1 modified"
+def fun1():
+	print "this is fun1 in f1"
 
-print "a=%s, b=%s, c=%s"%(a,b,c)
-fun()
+if __name__ == "__main__":
+	def fun2():
+		print "this is fun2 in f1"
+	print "__name__:",__name__
+	print __name__ == "__main__"
+	print "a=%s, b=%s, c=%s"%(a,b,c)
+	fun()
